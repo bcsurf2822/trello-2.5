da4cdbc6f2bff50b5155e848b79d0f3065c76ad3
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-11-19 12:22:25 -0500

    connect to db

---
+++ app/api/user/route.js
@@ -0,0 +1,36 @@
+// import connectToDatabase from "@/lib/mongooseConnect";
+// import User from "@/lib/models";
+
+// export async function GET(request) {
+//   try {
+//     await connectToDatabase();
+
+//     const users = await User.find({}); // Fetch all users
+//     return new Response(JSON.stringify(users), {
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   } catch (error) {
+//     return new Response(JSON.stringify({ error: error.message }), {
+//       status: 500,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   }
+// }
+
+// export async function POST(request) {
+//   try {
+//     await connectToDatabase();
+
+//     const data = await request.json();
+//     const newUser = await User.create(data); // Create a new user
+
+//     return new Response(JSON.stringify(newUser), {
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   } catch (error) {
+//     return new Response(JSON.stringify({ error: error.message }), {
+//       status: 500,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   }
+// }
