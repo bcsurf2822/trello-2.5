660e0a2deac0b43b36b3fb901895648894dd76a8
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-12-20 09:25:03 -0500

    fix name in mongoose.js for env var

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,21 +1,6 @@
 import connectToDatabase from "../../../lib/mongoose";
 import User from "../../../lib/models";
 
-// export async function GET(request) {
-//   try {
-//     await connectToDatabase();
-
-//     const users = await User.find({}); // Fetch all users
-//     return new Response(JSON.stringify(users), {
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   } catch (error) {
-//     return new Response(JSON.stringify({ error: error.message }), {
-//       status: 500,
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   }
-// }
 
 export async function POST(request) {
   try {
