edc1b0a9ea27374d95ace57840177f178b8affdb
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-12-22 13:32:17 -0500

    made conditional for empy list

--- app/dashboard/board/[id]/page.js
+++ app/dashboard/board/[id]/page.js
@@ -21,27 +21,24 @@ const getBoard = async (boardId) => {
 };
 
 export default async function BoardPage({ params }) {
-  const empyArray = [];
-  console.log("Dummy", dummyBoardData);
   const { id } = params;
-
+  const empyArray = {lists: []};
+  console.log("Dummy", dummyBoardData);
   const board = await getBoard(id);
 
   return (
     <main className="flex flex-col gap-2">
       <h1 className="text-3xl font-bold">{board?.name}</h1>
       <section className="flex gap-4 justify-between mx-2 ">
-        {dummyBoardData.lists.map((list) => (
-          <List key={list._id} list={list} />
-        ))}
+        {dummyBoardData.lists.length > 0 &&
+          dummyBoardData.lists.map((list) => (
+            <List key={list._id} list={list} />
+          ))}
 
         <div className="bg-green-100 pb-2 rounded-lg ">
-
-            <button className=" text-black w-full py-2 hover:bg-slate-400 flex items-center justify-center">
-              Add List
-
-            </button>
-  
+          <button className=" text-black w-full py-2 hover:bg-slate-400 flex items-center justify-center">
+            Add List
+          </button>
         </div>
       </section>
     </main>
