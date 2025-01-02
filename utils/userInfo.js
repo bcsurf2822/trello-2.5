c627149352f7fd718336796c400ad2c128acdd06
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-01-02 11:35:02 -0500

    deploy test

--- utils/userInfo.js
+++ utils/userInfo.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const fetchUserInfo = async () => {
   try {
-    // Construct the full URL for the API route
     const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
     const response = await axios.get(`${baseURL}/api/user`);
     console.log("Res", response);
