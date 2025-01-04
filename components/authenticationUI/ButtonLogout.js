b2794b934e5c4203b82c38ace406a53f49e334f1
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-01-04 13:42:42 -0500

    use effect to monitor guest state

--- components/authenticationUI/ButtonLogout.js
+++ components/authenticationUI/ButtonLogout.js
@@ -1,25 +1,29 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLogoutGuest } from "@/hooks/useLogoutGuest";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const ButtonLogout = ({ guestUser }) => {
-  const [guest, setGuest] = useState(guestUser|| null); // Manage guestId state locally
+  const [guest, setGuest] = useState(guestUser || null); // Manage guest state locally
   const logoutGuest = useLogoutGuest();
   const router = useRouter();
 
-  console.log("GuestUser", guest); // Log the full guestUser object
-
+  // Log guest state whenever it changes
+  useEffect(() => {
+    console.log("Guest state updated:", guest);
+  }, [guest]);
 
   const handleLogout = () => {
-    if (guest._id) {
+    if (guest?._id) {
       logoutGuest.mutate(
-        { guestId: guestUser._id },
+        { guestId: guest._id },
         {
           onSuccess: () => {
-            console.log("Guest successfully logged out. Resetting guestId to null...");
-            setGuest(null); // Reset guestId to null after successful logout
+            console.log(
+              "Guest successfully logged out. Resetting guest to null..."
+            );
+            setGuest(null); // Reset guest to null after successful logout
             router.push("/");
           },
         }
@@ -33,7 +37,9 @@ const ButtonLogout = ({ guestUser }) => {
   return (
     <button
       onClick={handleLogout}
-      className={`btn btn-error font-bold ${logoutGuest.isLoading ? "loading" : ""}`}
+      className={`btn btn-error font-bold ${
+        logoutGuest.isLoading ? "loading" : ""
+      }`}
       disabled={logoutGuest.isLoading}
     >
       {logoutGuest.isLoading ? "Logging out..." : "Logout"}
