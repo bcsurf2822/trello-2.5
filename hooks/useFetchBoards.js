bd8daac28a9958c4466042e7bf5f14a14dc9d04f
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-01-05 11:16:03 -0500

    accidentally messed with user route and was able to revert back to the guest being able to log in and out

--- hooks/useFetchBoards.js
+++ hooks/useFetchBoards.js
@@ -3,16 +3,18 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const useFetchBoards = () => {
-    const {guestId} = useGuest()
-    console.log("Guest ID in Hook", guestId)
+  const { guestId } = useGuest();
+  console.log("Guest ID in Hook", guestId);
 
   return useQuery({
     queryKey: ["boards"],
     queryFn: async () => {
-      const response = await axios.get("/api/boards");
+      const response = await axios.get("/api/boards", {
+        headers: { "Guest-ID": guestId || "" },
+      });
       return response.data;
     },
-    staleTime: 60 * 1000, 
-    cacheTime: 5 * 60 * 1000, 
+    staleTime: 60 * 1000, // 1 minute
+    cacheTime: 5 * 60 * 1000, // 5 minutes
   });
 };
