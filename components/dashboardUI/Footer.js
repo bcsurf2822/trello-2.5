b55129afae2c61e093805d36e4fe43ee0460118a
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-01-07 15:12:27 -0500

    added footer to project

---
+++ components/dashboardUI/Footer.js
@@ -0,0 +1,18 @@
+"use client";
+import { FaLinkedin } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="w-full flex gap-4 bg-gray-600 text-white py-2 text-center justify-center items-center">
+      <p>Developed by Benjamin Corbett</p>
+      <a
+        href="https://www.linkedin.com/in/your-linkedin-profile/" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="text-white hover:text-blue-400"
+      >
+        <FaLinkedin size={24} /> 
+      </a>
+    </footer>
+  );
+}
