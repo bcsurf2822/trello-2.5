225dc01a53c6ea596908d0a89b2b61a5876165b1
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-01-07 14:13:56 -0500

    accidentally deleted PUT route, I re added it trying to use this route

--- app/api/list/route.js
+++ app/api/list/route.js
@@ -121,3 +121,54 @@ export async function DELETE(req) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
+
+
+export async function PUT(req) {
+  try {
+    const body = await req.json();
+
+    if (!body.boardId || !body.lists) {
+      return NextResponse.json(
+        { error: "Board ID and lists are required" },
+        { status: 400 }
+      );
+    }
+
+    await connectMongo();
+
+    const session = await auth();
+
+    let user;
+
+    if (session) {
+      user = await User.findById(session.user.id);
+    } else {
+      user = await User.findOne({ isGuest: true });
+    }
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const board = await Board.findOne({
+      _id: body.boardId,
+      userId: user.id,
+    });
+
+    if (!board) {
+      return NextResponse.json({ error: "Board not found" }, { status: 404 });
+    }
+
+    board.lists = body.lists;
+
+    await board.save();
+
+    return NextResponse.json({
+      message: "Lists reordered successfully",
+      board,
+    });
+  } catch (error) {
+    console.error("Error saving list order:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
