5e0dc0bc30e8e2710a4c426ef8e0985d230fa4c3
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-01-20 14:37:38 -0500

    fixed logout UI

--- components/dashboardUI/NavBar.js
+++ components/dashboardUI/NavBar.js
@@ -14,8 +14,6 @@ export default async function NavBar() {
     guestUser = await fetchGuestInfo(guestId);
   }
 
-  console.log("Guest", guestUser)
-
   return (
     <nav className="navbar bg-neutral-300 ">
       <div className="navbar-start">
